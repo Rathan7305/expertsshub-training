cd300aebe220330a29db3b2ef06023062433cbcd
Rathan7305 <93189306+Rathan7305@users.noreply.github.com>
2021-10-26 15:23:17 +0530

    Add files via upload

---
+++ POINTER.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int a =50;
+int *p=&a;
+
+void main()
+
+{
+
+	   clrscr();
+	   printf("the value of p poiter is %d",*p);
+	   printf("\n address of p pointer is %x",p);
+
+
+getch();
+}
